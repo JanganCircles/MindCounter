ea12c7e0c020fb2a5fa257b40e5ddaf3afddf5d6
Nichi72 <money970709@naver.com>
2017-02-09 13:34:10 +0900

    StartPlus
    비빔

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,12 +6,14 @@ public class Move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        int abc = 72;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.up * Time.deltaTime*16f);
+
+
+        transform.Translate(Vector3.up * Time.deltaTime*8f);
 
 	}
 }
