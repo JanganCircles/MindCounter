37e05dfbe3c1e4c8e39606f8aabfe76c37b78b6f
dusdnr33 <qewrytu@naver.com>
2017-02-09 13:43:07 +0900

    속도
    바꿈

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,14 +6,12 @@ public class Move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        int abc = 72;
+        int abc = 144;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-        transform.Translate(Vector3.up * Time.deltaTime*8f);
+        transform.Translate(Vector3.up * Time.deltaTime*72f);
 
 	}
 }
