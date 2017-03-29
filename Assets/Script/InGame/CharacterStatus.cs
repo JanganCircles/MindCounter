7a0e366d60f62d2646d398d1503219a624d0649d
dusdnr33 <qewrytu@naver.com>
2017-03-30 03:40:44 +0900

    캐선창변경

--- Assets/Script/InGame/CharacterStatus.cs
+++ Assets/Script/InGame/CharacterStatus.cs
@@ -18,12 +18,22 @@ public class CharacterStatus : MonoBehaviour {
     public const int ROCK   = 0;
     public const int SCISSOR = 1;
     public const int PAPER  = 2;
+
+    private bool isYellowCanSee;
     void Reset()
     {
+        isYellowCanSee = false;
         Cost = (MaxCost = CharacterData.Mp) / 2;
         Guard = false;
         SetMaxHP(false, CharacterData.Hp);
         WallDistance = 600;
+    }
+    void Start()
+    {
+        Vector2 Hps = new Vector2();
+        Hps.x = HP;
+        Hps.y = MaxHP;
+        UIProgressBar.SetData((Controller == 0 ? "Champion" : "Challanger") + "HP", Hps);
     }
     public void HpDown_Debuff(int Damage)
     {
@@ -79,15 +89,18 @@ public class CharacterStatus : MonoBehaviour {
         Hps.y = MaxHP;
         UIProgressBar.SetData(ProgressBarName, Hps);
         ProgressBarName += "Y";
-
+        if (isYellowCanSee)
+            yield break;
+        isYellowCanSee = true;
         Debug.Log("노란x"+YellowHps);
         Debug.Log("빨간x" + Hps);
-        while (YellowHps.x > Hps.x)
+        while (YellowHps.x > HP)
         {
             Debug.Log("여기 드어옵니까");
             YellowHps.x--;
             UIProgressBar.SetData(ProgressBarName, YellowHps);
             yield return null;
         }
+        isYellowCanSee = false;
     }
 }
