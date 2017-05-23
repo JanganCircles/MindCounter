b1dbdcb0a4d82190b52ea495748e1a9e55d64b75
gurtn08 <gurtn08@naver.com>
2017-05-24 03:41:51 +0900

    소리매니저 완

---
+++ Assets/Script/CharacterSelectUI/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour {
+    private AudioSource ThisSource;
+    public AudioClip[] ClipList;
+    private Dictionary<string, AudioClip> dicClip;
+    public static SoundManager ins = null;
+	// Use this for initialization
+	void Awake () {
+        if (ins == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            ins = this;
+        }
+        else
+        {
+            AddClip(ClipList);
+            Destroy(gameObject);
+        }
+        ThisSource = GetComponent<AudioSource>();
+
+        dicClip = new Dictionary<string, AudioClip>();
+        for (int i = 0; i < ClipList.Length; i++)
+        {
+            dicClip.Add(ClipList[i].name, ClipList[i]);
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void AddClip(AudioClip clip)
+    {
+        if (!dicClip.ContainsKey(clip.name))
+        {
+            dicClip.Add(clip.name, clip);
+        }
+    }
+     static void AddClip(AudioClip[] clips)
+    {
+        for(int i = 0; i < clips.Length;i++)
+            SoundManager.ins.AddClip(clips[i]);
+    }
+    public void RunAudio(string Name)
+    {
+        if (dicClip.ContainsKey(Name)) 
+        ThisSource.PlayOneShot(dicClip[Name]);
+
+    }
+}
